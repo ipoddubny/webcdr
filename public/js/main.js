0105514724e9fdb9cd33599ff1286667e8a36b93
Ivan Poddubny <ivan.poddubny@gmail.com>
2014-06-06 06:11:40 +0000

    backgrid, bootstrap, marionette start

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,33 @@
 
+var app = new Backbone.Marionette.Application();
+
+var superView = Marionette.ItemView.extend({
+  template: _.template('<div>hello once again</div>'),
+});
+
+var mainRegion = Marionette.Region.extend({
+  el: '#container'
+});
+
+$(function () {
+
+  app.addRegions({
+    mainRegion: mainRegion
+  });
+
+  app.addInitializer(function () {
+    console.log('hello');
+    var myView = new superView();
+    myView.render();
+    console.log(myView.el);
+    app.mainRegion.show(myView);
+  });
+
+  app.on("initialize:after", function(options){
+    if (Backbone.history){
+      Backbone.history.start();
+    }
+  });
+
+  app.start();
+});
