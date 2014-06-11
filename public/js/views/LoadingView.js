2713e460ed0b9a50b425f43c5d0c28640766df51
Ivan Poddubny <ivan.poddubny@gmail.com>
2014-06-11 07:03:54 +0000

    Красивый лоадер (spin.js)

--- public/js/views/LoadingView.js
+++ public/js/views/LoadingView.js
@@ -1,6 +1,27 @@
 var LoadingView = Marionette.ItemView.extend({
-  template: _.template('загрузка...'),
-  className: 'container'
+  template: _.template(''),
+  className: 'container',
+  onRender: function () {
+    var opts = {
+      lines: 13, // The number of lines to draw
+      length: 20, // The length of each line
+      width: 10, // The line thickness
+      radius: 30, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#000', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: false, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '50%', // Top position relative to parent
+      left: '50%' // Left position relative to parent
+    };
+    var spinner = new Spinner(opts).spin(this.el);
+  }
 });
 
 module.exports = LoadingView;
