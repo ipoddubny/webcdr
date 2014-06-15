45f494eca83cac3102c174d4d7219fed358c2c5a
Ivan Poddubny <ivan.poddubny@gmail.com>
2014-06-15 06:48:57 +0000

    Authentication with passport.js
    Local strategy only, predefined list of users in source.
    Login page is pretty basic.
    /api route handler moved to api.js.

---
+++ api.js
@@ -0,0 +1,95 @@
+var router = require('express').Router();
+
+var Bookshelf = require('bookshelf').db;
+var Promise = require('bluebird');
+var moment = require('moment');
+var _ = require('lodash');
+var ExcelExport = require('excel-export');
+
+var CDR = Bookshelf.Model.extend({
+  tableName: 'cdr'
+});
+
+function prepareXlsx (collection) {
+  var conf = {};
+  conf.cols = [{
+    caption: 'Дата и время',
+    type: 'date',
+    beforeCellWrite: function () {
+      var originDate = new Date(Date.UTC(1899, 11, 30));
+      return function (row, cellData, eOpt) {
+        console.log(cellData, typeof cellData);
+        return (cellData - originDate) / (24 * 60 * 60 * 1000);
+      };
+    }()
+  }, {
+    caption: 'Кто звонил',
+    type: 'string'
+  }, {
+    caption: 'Куда звонил',
+    type: 'string'
+  }];
+  conf.rows = collection.map(function (model) {
+    return [model.get('calldate'), model.get('src'), model.get('dst')];
+  });
+  return ExcelExport.execute(conf);
+}
+
+router.get('/cdrs', function (req, res) {
+  console.log('query', req.query);
+  var page = parseInt(req.query.page, 10);
+  var perPage = parseInt(req.query.per_page, 10);
+
+  var filter = function () {
+    this.where(function () {
+      if (req.query.number) {
+        var like =['%',req.query.number,'%'].join('');
+        this.where('src', 'like', like)
+            .orWhere('dst', 'like', like);
+      } else {
+        this.whereRaw('1=1');
+      }
+    }).andWhere(function () {
+      if (req.query.status) {
+        this.whereIn('disposition', req.query.status);
+      } else {
+        this.whereRaw('1=1');
+      }
+    }).andWhere(function () {
+      this.whereBetween('calldate', [
+        req.query.start || moment().startOf('day').toJSON(),
+        req.query.end || moment().endOf('day').toJSON()
+      ]);
+    });
+  };
+
+  var countPromise = Bookshelf.knex('cdr').count('*');
+  filter.call(countPromise);
+
+  var dataPromise = CDR.collection()
+    .query(filter)
+    .query(function (qb) {
+      if (page && perPage) {
+        qb.offset((page - 1) * perPage);
+        qb.limit(perPage);
+      }
+      qb.orderBy(req.query.sort_by || 'calldate', req.query.order || 'desc');
+    })
+    .fetch();
+
+  Promise.all([countPromise, dataPromise]).spread(function (count, collection) {
+    var result;
+    if (req.query.export === 'xlsx') {
+      result = prepareXlsx(collection);
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+      res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
+      res.end(result, 'binary');
+      return;
+    };
+    var cnt = count[0]['count(*)'];
+    result = [{total_entries: cnt}, collection.toJSON()];
+    res.json(result);
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-var Promise = require('bluebird');
-var moment = require('moment');
-var _ = require('lodash');
-
-var ExcelExport = require('excel-export');
-
 var express = require('express');
 var compress = require('compression');
-var app = express();
-
-app.use(compress());
-app.use(express.static(__dirname + '/public'));
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var Bookshelf = require('bookshelf');
 Bookshelf.db = Bookshelf.initialize({
@@ -22,90 +16,80 @@ Bookshelf.db = Bookshelf.initialize({
   }
 });
 
-var CDR = Bookshelf.db.Model.extend({
-  tableName: 'cdr'
+var _ = require('lodash');
+var testUsers = [{
+  id: 1,
+  username: 'admin',
+  password: 'admin'
+}, {
+  id: 2,
+  username: 'test',
+  password: 'test'
+}];
+
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
 });
-
-function prepareXlsx (collection) {
-  var conf = {};
-  conf.cols = [{
-    caption: 'Дата и время',
-    type: 'date',
-    beforeCellWrite: function () {
-      var originDate = new Date(Date.UTC(1899, 11, 30));
-      return function (row, cellData, eOpt) {
-        console.log(cellData, typeof cellData);
-        return (cellData - originDate) / (24 * 60 * 60 * 1000);
-      };
-    }()
-  }, {
-    caption: 'Кто звонил',
-    type: 'string'
-  }, {
-    caption: 'Куда звонил',
-    type: 'string'
-  }];
-  conf.rows = collection.map(function (model) {
-    return [model.get('calldate'), model.get('src'), model.get('dst')];
-  });
-  return ExcelExport.execute(conf);
-}
-
-app.get('/api/cdrs', function (req, res) {
-  console.log('query', req.query);
-  var page = parseInt(req.query.page, 10);
-  var perPage = parseInt(req.query.per_page, 10);
-
-  var filter = function () {
-    this.where(function () {
-      if (req.query.number) {
-        var like =['%',req.query.number,'%'].join('');
-        this.where('src', 'like', like)
-            .orWhere('dst', 'like', like);
-      } else {
-        this.whereRaw('1=1');
-      }
-    }).andWhere(function () {
-      if (req.query.status) {
-        this.whereIn('disposition', req.query.status);
-      } else {
-        this.whereRaw('1=1');
-      }
-    }).andWhere(function () {
-      this.whereBetween('calldate', [
-        req.query.start || moment().startOf('day').toJSON(),
-        req.query.end || moment().endOf('day').toJSON()
-      ]);
-    });
-  };
-
-  var countPromise = Bookshelf.db.knex('cdr').count('*');
-  filter.call(countPromise);
-
-  var dataPromise = CDR.collection()
-    .query(filter)
-    .query(function (qb) {
-      if (page && perPage) {
-        qb.offset((page - 1) * perPage);
-        qb.limit(perPage);
-      }
-      qb.orderBy(req.query.sort_by || 'calldate', req.query.order || 'desc');
-    })
-    .fetch();
-
-  Promise.all([countPromise, dataPromise]).spread(function (count, collection) {
-    var result;
-    if (req.query.export === 'xlsx') {
-      result = prepareXlsx(collection);
-      res.setHeader('Content-Type', 'application/vnd.openxmlformats');
-      res.setHeader("Content-Disposition", "attachment; filename=" + "Report.xlsx");
-      res.end(result, 'binary');
-      return;
-    };
-    var cnt = count[0]['count(*)'];
-    result = [{total_entries: cnt}, collection.toJSON()];
-    res.json(result);
+passport.deserializeUser(function (id, done) {
+  process.nextTick(function () {
+    var user = _.find(testUsers, {id: id});
+    done(null, user);
   });
 });
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    var user = _.find(testUsers, {username: username, password: password});
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false, { message: "Wrong username or password" });
+    }
+  }
+));
+
+var app = express();
+app.use(morgan('dev'));
+app.use(compress());
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(session({
+  secret: '123hjhfds7&&&kjfh&&&788'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function (req, res) {
+  res.sendfile('./public/login.html');
+});
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
+);
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+app.use('/api', ensureAuthenticated);
+app.use('/api', require('./api'));
+
+app.get('/', ensureAuthenticated);
+app.get('/', function (req, res) {
+  res.sendfile('./public/index.html');
+});
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 9030);
+
+function ensureAuthenticated (req, res, next) {
+  if (!req.isAuthenticated()) {
+    res.redirect('/login');
+  }
+  next();
+}
