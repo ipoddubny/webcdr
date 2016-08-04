b2a4f235ea9bc2fdfcb3f1346f8a48af6b463df6
Ivan Poddubny <ivan.poddubny@gmail.com>
2016-08-04 19:26:56 +0200

    Add danish locale

---
+++ locales/da.js
@@ -0,0 +1,82 @@
+module.exports = {
+  // login page
+  'User': 'Brugernavn',
+  'Password': 'Adgangskode',
+  'Log in': 'Log på',
+
+  // main
+  'Calls': 'Opkald',
+  'Users': 'Brugere',
+
+  // navbar
+  'Show menu': 'Vis menu',
+  'Log out': 'Log ud',
+
+  // cdr/CDRView.js
+  'No answer': 'Intet svar',
+  'Busy': 'Optaget',
+  'Answered': 'Svarede',
+  'Failed': 'Fejl',
+  'Congestion': 'Overbelastning',
+
+  'Time': 'Dato og tid',
+  'Source': 'Kilde',
+  'Destination': 'Destination',
+  'Status': 'Status',
+  'Talking time': 'Taletid',
+  'Recording': 'Optagelse',
+
+  'Download': 'Hent',
+  'call recordings': 'opkald optagelser',
+  'spreadsheet': 'regneark',
+
+  // cdr/FilterView.js
+  'Apply': 'Anvend',
+  'Cancel': 'Annuler',
+  'From': 'Fra',
+  'To': 'Til',
+  'Custom': 'Tilpas',
+
+  'Today': 'I dag',
+  'Yesterday': 'I går',
+  'Last 7 days': 'Sidste 7 dage',
+  'Last 30 days': 'Sidste 30 dage',
+  'This month': 'Denne måned',
+  'Last month': 'Sidste måned',
+
+  'All': 'Alle',
+
+  // filter.html
+
+  'Direction': 'Retning',
+  'Inbound': 'Indgående',
+  'Outbound': 'Udgående',
+  'Internal': 'Interne',
+
+  'Phone number': 'Telefonnummer',
+
+  'Results per page': 'Resultater per side',
+
+  // admin/AdminView.js
+
+  'Create': 'Opret',
+  'New user': 'Ny bruger',
+  'Delete user #?': 'Slet bruger #?',
+  'Edit user': 'Regider bruger',
+
+  // admin/table.html
+  'Name': 'Navn',
+  'Username': 'Brugernavn',
+  'Administrator': 'Administrator',
+  'Created': 'Oprettet',
+
+  // admin/usermodal.html
+  'Close': 'Lukket',
+  'Joe User': 'Joe User',
+  'joeuser': 'joeuser',
+
+  'Use AD to auth': 'Brug AD at autentificere',
+  'Limit access to calls of these extensions': 'Begræns adgangen til opkald af disse telefoner',
+  'Show all incoming calls': 'Vis alle indgående opkald',
+  'Save': 'Gem',
+};
