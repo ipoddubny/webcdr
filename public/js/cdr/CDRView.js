a30ad88a47f38e376a5ee08c28f23107edebe755
Ivan Poddubny <ivan.poddubny@gmail.com>
2017-01-28 19:56:26 +0100

    Display dstchannel column
    It's a rather common case in Asterisk to have a Dial calling
    multiple destinations:
    
    exten => _1234567,n,Dial(SIP/101&SIP/102&SIP/103)
    
    Asterisk 12+ creates multiple records for the call, all of them
    having "1234567" in dst, so having only src and dst it's impossible
    to figure out which of the SIP phones answered.
    
    The only column that has this information is dstchannel,
    so it is reasonable to have in the table by default.
    
    Closes #6

--- public/js/cdr/CDRView.js
+++ public/js/cdr/CDRView.js
@@ -72,6 +72,11 @@ var columns = [{
   editable: false,
   cell: 'string'
 }, {
+  name: 'dstchannel',
+  label: $$('Dstchannel'),
+  editable: false,
+  cell: 'string'
+}, {
   name: 'disposition',
   label: $$('Status'),
   editable: false,
