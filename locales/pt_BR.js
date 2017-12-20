b4386e5c8548c448cd8797146a403cc73d5d0477
Mateus Godoi Milanez <milaneza@gmail.com>
2017-12-20 13:33:05 -0200

    Added locale for language pt-BR (#16)

---
+++ locales/pt_BR.js
@@ -0,0 +1,82 @@
+module.exports = {
+  // login page
+  'User': 'Usuário',
+  'Password': 'Senha',
+  'Log in': 'Log in',
+
+  // main
+  'Calls': 'Chamadas',
+  'Users': 'Usuários',
+
+  // navbar
+  'Show menu': 'Show menu',
+  'Log out': 'Log out',
+
+  // cdr/CDRView.js
+  'No answer': 'Não atendidas',
+  'Busy': 'Ocupado',
+  'Answered': 'Atendido',
+  'Failed': 'Com falha',
+  'Congestion': 'Acumulada',
+
+  'Time': 'Data e hora',
+  'Source': 'Origem',
+  'Destination': 'Destino',
+  'Status': 'Status',
+  'Talking time': 'Horário da chamada',
+  'Recording': 'Gravação',
+
+  'Download': 'Download',
+  'call recordings': 'gravações de chamadas',
+  'spreadsheet': 'planilha',
+
+  // cdr/FilterView.js
+  'Apply': 'Aplicar',
+  'Cancel': 'Cancela',
+  'From': 'De',
+  'To': 'Para',
+  'Custom': 'Personalizado',
+
+  'Today': 'Hoje',
+  'Yesterday': 'Ontem',
+  'Last 7 days': 'Últimos 7 dias',
+  'Last 30 days': 'Últimos 30 dias',
+  'This month': 'Este mês',
+  'Last month': 'Mês passado',
+
+  'All': 'Todos',
+
+  // filter.html
+
+  'Direction': 'Direção',
+  'Inbound': 'Entrada',
+  'Outbound': 'Saída',
+  'Internal': 'Intena',
+
+  'Phone number': 'Número do telefone',
+
+  'Results per page': 'Resultados por página',
+
+  // admin/AdminView.js
+
+  'Create': 'Criado',
+  'New user': 'Novo usuário',
+  'Delete user #?': 'Deseja realmente deletar o usuário #?',
+  'Edit user': 'Editar usuário',
+
+  // admin/table.html
+  'Name': 'Nome',
+  'Username': 'Usuário',
+  'Administrator': 'Administrador',
+  'Created': 'Criado',
+
+  // admin/usermodal.html
+  'Close': 'Fechado',
+  'Joe User': 'Joe User',
+  'joeuser': 'joeuser',
+
+  'Use AD to auth': 'Use AD para autenticação',
+  'Limit access to calls of these extensions': 'Limite o acesso para chamadas apenas com os números',
+  'Show all incoming calls': 'Ver todas as chamadas entrantes',
+  'Save': 'Salvar'
+};
